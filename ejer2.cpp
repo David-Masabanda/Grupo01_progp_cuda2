392a7a616f896bfdc61735a4fc30b20dad6b4a8a
David-Masabanda <josue.masabanda99@gmail.com>
2024-02-22 00:01:42 -0500

    Pruebas

--- ejer2.cpp
+++ ejer2.cpp
@@ -2,12 +2,26 @@
 #include <chrono>
 #include <fmt/core.h>
 #include <cuda_runtime.h>
+#include <fstream>
+
 
 extern "C" void cuda_scanPrefixSum(const int* input, int* output, int n);
 
+std::vector<int> read_file() {
+    std::fstream fs("C:/Users/josue/Desktop/Universidad/ProgParalela/Grupal_final/numeros4.txt", std::ios::in);
+    std::string line;
+    std::vector<int> ret;
+    while (std::getline(fs, line)) {
+        ret.push_back(std::stoi(line));
+    }
+    fs.close();
+    return ret;
+}
+
 int main() {
     // Ejemplo de uso
-    std::vector<int> input = {10, 20, 10, 5, 15,12,118,59,64,75,216,84,262,84,4,15,7};
+    //std::vector<int> input = {10, 20, 10, 5, 15,12,118,59,64,75,216,84,262,84,4,15,7};
+    std::vector<int> input = read_file();
     std::vector<int> output(input.size());
 
     auto start = std::chrono::high_resolution_clock::now();
@@ -30,17 +44,17 @@ int main() {
     cudaFree(d_output);
 
     // Imprimir el resultado
-    fmt::print("Input: ");
-    for (const auto& value : input) {
-        fmt::print("{} ", value);
-    }
-    fmt::print("\n");
-
-    fmt::print("Prefix Sum: ");
-    for (const auto& value : output) {
-        fmt::print("{} ", value);
-    }
-    fmt::print("\n");
+//    fmt::print("Input: ");
+//    for (const auto& value : input) {
+//        fmt::print("{} ", value);
+//    }
+//    fmt::print("\n");
+//
+//    fmt::print("Prefix Sum: ");
+//    for (const auto& value : output) {
+//        fmt::print("{} ", value);
+//    }
+//    fmt::print("\n");
 
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::milli> tiempo = end - start;
